0e47d0d5e0ccef1620636146691fca635e0e1914
ndeans <ndeans@hotmail.com>
2026-03-15 00:40:21 -0700

    initial commit

---
+++ src/test/java/us/deans/raven/AppTest.java
@@ -0,0 +1,13 @@
+package us.deans.raven;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void appStarts() {
+        assertTrue(true);
+    }
+}
